efc122b274d8c40c07b78efd3d862240a7dd6167
HanSunKyu <bakha1105@gmail.com>
2018-04-29 18:13:58 +0900

    test commit

--- src/main/java/com/cnu/blackjack/Application.java
+++ src/main/java/com/cnu/blackjack/Application.java
@@ -1,4 +1,15 @@
 package com.cnu.blackjack;
 
 public class Application {
+    public static void main(String[] args) {
+        Game game = new Game(new Deck(2));
+        game.addPlayer("한선규", 4000);
+        game.addPlayer("정성철", 6000);
+        game.placeBet("한선규", 2000);
+        game.placeBet("정성철", 3000);
+        game.start();
+        Evaluator evaluator = new Evaluator(game.getPlayerList());
+        evaluator.start();
+    }
+
 }
--- src/test/java/com/cnu/blackjack/EvaluatorTest.java
+++ src/test/java/com/cnu/blackjack/EvaluatorTest.java
@@ -1,17 +1,49 @@
 package com.cnu.blackjack;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class EvaluatorTest {
 
+    Game game;
+
+    @Before
+    public void setUp(){
+        game = new Game(new Deck(2));
+        //2장의 카드덱으로 게임을 진행
+    }
+
     @Test
     public void 게임초기화시_모든플레이어는_2장의카드를_받는다() {
-
+        //모든플레이어이므로 2인이상의 플레이어로 테스트
+        game.addPlayer("player1", 3000);
+        game.addPlayer("player2", 5000);
+        Evaluator evaluator = new Evaluator(game.getPlayerList());
+        Player player1 = game.getPlayerList().get("player1");
+        Player player2 = game.getPlayerList().get("player2");
+        assertThat(player1.getHand().getCardList().size(), is(2));
+        assertThat(player2.getHand().getCardList().size(), is(2));
     }
 
     @Test
     public void 각_플레이어는_16이하면_히트한다() {
+        game.addPlayer("player1", 3000);
+        Evaluator evaluator = new Evaluator(game.getPlayerList());
+        Player player1 = game.getPlayerList().get("player1");
+        int player1_score = evaluator.getPlayerScore(player1);
 
+        if(player1_score <= 16) {
+            player1.hitCard();
+            assertThat(player1.getHand().getCardList().size(), is(3));
+        }else{
+            assertThat(player1.getHand().getCardList().size(), is(2));
+        }
     }
 
     @Test
@@ -21,6 +53,17 @@ public class EvaluatorTest {
 
     @Test
     public void 각_플레이어는_17이상이면_스테이한다() {
-
+        game.addPlayer("player1", 3000);
+        Evaluator evaluator = new Evaluator(game.getPlayerList());
+        game.getPlayerList().forEach((name, player)-> {
+            //17이상일 경우에는 stay했으므로 카드가 2장일 것이다.
+           if(evaluator.getPlayerScore(player) >= 17){
+               assertThat(player.getHand().getCardList().size(), is(2));
+           }
+           //hit했을때는 카드가 3장이다.
+            else{
+               assertThat(player.getHand().getCardList().size(), is(2));
+           }
+        });
     }
 }
